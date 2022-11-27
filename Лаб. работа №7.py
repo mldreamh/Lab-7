55a57086ea91c38a17a148a08e22967eb8098b36
margarita lebedkina <113675455+mldreamh@users.noreply.github.com>
2022-11-27 11:46:42 +0500

    Update Лаб. работа №7.py

--- Лаб. работа №7.py
+++ Лаб. работа №7.py
@@ -1,183 +1,315 @@
-import tkinter as tk
-from tkinter import Label
-''
-print('x — номер вертикали (слева направо), y — номер горизонтали (снизу вверх)\
-x и y должны быть в отрезке [1; 8]\nВведите координаты первой клетки')
+import numpy as np 
+import logging
 
+logging.basicConfig(filename = "base_log.log", level=logging.INFO)
+
+# Ввод данных полей
 while True:
-    try:
-        x0 = int(input('Введите x0  '))
-        assert 1 <= x0 <= 8
+    k = int(input('Введите первое число по вертикали:')) - 1
+    l = int(input('Введите первое число по горизонтали:')) - 1
+    m = int(input('Введите второе число по вертикали:')) - 1
+    n = int(input('Введите второе число по горизонтали:')) - 1
+    if  0 > k or k > 8 or 0 > l or l > 8 or 0 > m or m > 8  or 0 > n or n > 8:
+        print('Введите значение в каждом поле от 0 до 8')
+        continue
+    else:
         break
-    except AssertionError:
-        print('Координата не в [1; 8]')
-    except ValueError:
-        print('Ошибка в значении')
 
-while True:
-    try:
-        y0 = int(input('Введите y0  '))
-        assert 1 <= y0 <= 8
-        break
-    except AssertionError:
-        print('Координата не в [1; 8]')
-    except ValueError:
-        print('Ошибка в значении')
+# Создание шахматного поля матричным способом (1 - черное, 0 - белое)
+area = np.full((8, 8), 1)
 
-print('Введите координаты второй клетки')
+for i in range(len(area)):
+    for j in range(len(area[i])):
+        if ((i + 1) % 2 == 0) and (j % 2 != 0):
+            area[i][j] -= 1
+        elif ((i + 1) % 2 != 0) and (j % 2 == 0):
+            area[i][j] -= 1
 
-while True:
-    try:
-        x1 = int(input('Введите x1  '))
-        assert 1 <= x1 <= 8
-        break
-    except AssertionError:
-        print('Координата не в [1; 8]')
-    except ValueError:
-        print('Ошибка в значении')
+# Вывод шахматного поля матричным способом
+print('1 - черное поле, 0 - белое поле, 9 - поле (k, l), 8 - поле (m, n)')
+area[7 - l][k] = 9
+area[7 - n][m] = 8
 
-while True:
-    try:
-        y1 = int(input('Введите y1  '))
-        assert 1 <= y1 <= 8
-        assert y0 != y1 or x0 != x1
-        break
-    except AssertionError:
-        print('Координата не в [1; 8] или вы ввели одинаковые клетки')
-    except ValueError:
-        print('Ошибка в значении')
+print(area)
 
-def color_check(x0, y0, x1, y1):
-    if (x0 + y0) % 2 == (x1 + y1) % 2:
-        return True
-    else:
-        return False
-
-
-def knight_check(x0, y0, x1, y1):
-    if (x1 - x0) ** 2 + (y1 - y0) ** 2 == 5:
-        return True
-    else:
-        return False
-
-
-def rook_check(x0, y0, x1, y1):
-    if x0 == x1 or y0 == y1:
-        return True
-    else:
-        return False
-
-
-def bishop_check(x0, y0, x1, y1):
-    if (x1 - x0) ** 2 == (y1 - y0) ** 2:
-        return True
-    else:
-        return False
-
-
-def queen_check(x0, y0, x1, y1):
-    if rook_check(x0, y0, x1, y1) or bishop_check(x0, y0, x1, y1):
-        return True
-    else:
-        return False
-
-
-if color_check(x0, y0, x1, y1):
-    print('Клетки одного цвета')
+# Проверка 2-ух полей на сходимость цвета
+if area[7-l][k] == area[7-n][m]:
+    print('Поля имеют одинаковый цвет')
 else:
-    print('Клетки разного цвета')
+    print('Поля имеют разные цвета')
 
-# Рисуем доску
-figure = ''
-
-
-def draw_desk(x0, y0, x1, y1):
-    print('Откройте окно')
-    BRD_ROWS = BRD_COLS = 8
-    CELL_SZ = 50
-
-    root = tk.Tk()
-    root.title('Закройте окно для продолжения')
-    Label(text="начальная клетка - желтый, конечная клетка - зелёный, красный - промежуточное значение", width=100,
-          height=5) \
-        .pack()
-    canvas = tk.Canvas(root, width=CELL_SZ * BRD_ROWS, height=CELL_SZ * BRD_COLS)
-
-    cell_colors = ['white', 'black']
-    ci = 0  # цветовой индекс
-
-    # Рисует пустую доску
-    def draw_empty_desk():
-        if row == 8 - y0 and col == x0 - 1:
-            canvas.create_rectangle((x_0, y_0), (x_1, y_1), fill='yellow')
-        elif row == 8 - y1 and col == x1 - 1:
-            canvas.create_rectangle((x_0, y_0), (x_1, y_1), fill='green')
-        else:
-            canvas.create_rectangle((x_0, y_0), (x_1, y_1), fill=cell_colors[ci])
-
-    for row in range(BRD_ROWS):
-        for col in range(BRD_COLS):
-            x_0, y_0 = col * CELL_SZ, row * CELL_SZ
-            x_1, y_1 = col * CELL_SZ + CELL_SZ, row * CELL_SZ + CELL_SZ
-
-            if figure == 'ферзь' or figure == 'ладья':
-                if row == 8 - y0 and col == x1 - 1:
-                    canvas.create_rectangle((x_0, y_0), (x_1, y_1), fill='red')
-                else:
-                    draw_empty_desk()
-            elif figure == 'слон':
-                if bishop_check(x0, y0, 8-row, col+1) and bishop_check(x1, y1, 8-row, col+1):
-                    canvas.create_rectangle((x_0, y_0), (x_1, y_1), fill='red')
-                else:
-                    draw_empty_desk()
-            else:
-                draw_empty_desk()
-
-            ci = not ci
-
-        ci = not ci
-
-    canvas.pack()
-
-    root.mainloop()
-
-
-draw_desk(x0, y0, x1, y1)
-
+# Проверка на угрозу поля (m, n)
+name_list = ['ладья', 'конь', 'ферзь', 'слон']
 while True:
-    a = 1
-    print('True - ход возможен False - ход невозможен ')
-    figure = input('Введите название фигуры: ферзь/ладья/слон/конь  ')
-    if figure == 'ферзь':
-        if queen_check(x0, y0, x1, y1):
-            print('ход возможен')
-        else:
-            print('ход невозможен')
-            print('Реализация за два хода')
-            draw_desk(x0, y0, x1, y1)
-    elif figure == 'ладья':
-        if rook_check(x0, y0, x1, y1):
-            print('ход возможен')
-        else:
-            print('ход невозможен')
-            print('Реализация за два хода')
-            draw_desk(x0, y0, x1, y1)
-    elif figure == 'слон':
-        if bishop_check(x0, y0, x1, y1):
-            print('ход возможен')
-        elif color_check(x0, y0, x1, y1):
-            print('ход невозможен')
-            print('Реализация за два хода')
-            draw_desk(x0, y0, x1, y1)
-
-    elif figure == 'конь':
-        if knight_check(x0, y0, x1, y1):
-            print('ход возможен')
-        else:
-            print('ход невозможен')
+    name_area = input('Укажите наименование фигуры для поля (k, l) (ладья, конь, ферзь или слон): ').lower()
+    if name_area not in name_list:
+        print('Введите правильное название фигуры (ладья, конь, ферзь или слон)')
+        continue
     else:
-        a = 0
-        print('Неверная фигура')
-    if a:
         break
+
+def queen(x0, y0, x1, y1):
+    logging.info("Function call queen")
+    x = x0
+    y = y0
+
+    if (y0 == y1) or (x0 == x1):
+            return True
+    elif y0 < y1:
+        for i in range(y0, y1 + 1):
+            x0 += 1
+            y0 += 1
+            if x0 == x1 and y0 == y1:
+                return True
+    elif y0 > y1:
+        for i in range(y1, y0 + 1):
+            x0 -= 1
+            y0 -= 1
+            if x0 == x1 and y0 == y1:
+                return True
+    x0 = x
+    y0 = y
+    if x0 < x1:
+        for i in range(x0, x1 + 1):
+            x0 += 1
+            y0 -= 1
+            if x0 == x1 and y0 == y1:
+                return True
+    elif x0 > x1:
+        for i in range(x1, x0 + 1):
+            x0 -= 1
+            y0 += 1
+            if x0 == x1 and y0 == y1:
+                return True
+
+def rook(x0, y0, x1, y1):
+    logging.info("Function call rook")
+    if (y0 == y1) or (x0 == x1):
+            return True
+
+def eleph(x0, y0, x1, y1):
+    logging.info("Function call eleph")
+    x = x0
+    y = y0
+
+    if y0 < y1:
+        for i in range(y0, y1 + 1):
+            x0 += 1
+            y0 += 1
+            if x0 == x1 and y0 == y1:
+                return True
+    elif y0 > y1:
+        for i in range(y1, y0 + 1):
+            x0 -= 1
+            y0 -= 1
+            if x0 == x1 and y0 == y1:
+                return True
+    x0 = x
+    y0 = y
+    if x0 < x1:
+        for i in range(x0, x1 + 1):
+            x0 += 1
+            y0 -= 1
+            if x0 == x1 and y0 == y1:
+                return True
+                break
+    elif x0 > x1:
+        for i in range(x1, x0 + 1):
+            x0 -= 1
+            y0 += 1
+            if x0 == x1 and y0 == y1:
+                return True
+
+def horse(x0, y0, x1, y1):
+    logging.info("Function call horse")
+    if y0 + 2 == y1 and x0 - 1 == x1:
+        return True
+    elif y0 + 2 == y1 and x0 + 1 == x1:
+        return True
+    elif y0 - 2 == y1 and x0 - 1 == x1:
+        return True
+    elif y0 - 2 == y1 and x0 + 1 == x1:
+        return True
+    elif x0 + 2 == x1 and y0 + 1 == y1:
+        return True
+    elif x0 + 2 == x1 and y0 - 1 == y1:
+        return True
+    elif x0 - 2 == x1 and y0 + 1 == y1:
+        return True
+    elif x0 - 2 == x1 and y0 - 1 == y1:
+        return True
+
+# Проверка на угрозу
+if name_area == 'ферзь':
+    if queen(k, l, m, n):
+        print('Угрожает полю (m, n)')
+    else:
+        print('Не угрожает полю (m, n)')
+elif name_area == 'ладья':
+    if rook(k, l, m, n):
+        print('Угрожает полю (m, n)')
+    else:
+        print('Не угрожает полю (m, n)')
+elif name_area == 'слон':
+    if eleph(k, l, m, n):
+        print('Угрожает полю (m, n)')
+    else:
+        print('Не угрожает полю (m, n)')
+elif name_area == 'конь':
+    if horse(k, l, m, n):
+        print('Угрожает полю (m, n)')
+    else:
+        print('Не угрожает полю (m, n)')
+
+def queen_move(x0, y0, x1, y1):
+    logging.info("Function call queen_move")
+    x0 = x1
+    print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+    y0 = y1
+    if x0 == x1 and y0 == y1:
+        print('Фигура достигла поля (m, n)')
+    else:
+        print('Перемещение за один или два хода невозможно')
+
+def rook_move(x0, y0, x1, y1):
+    logging.info("Function call rook_move")
+    x0 = x1
+    print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+    y0 = y1
+    if x0 == x1 and y0 == y1:
+        print('Фигура достигла поля (m, n)')
+    else:
+        print('Перемещение за один или два хода невозможно')
+
+def eleph_move(x0, y0, x1, y1):
+    logging.info("Function call eleph_move")
+    for i in range(8):
+        x0 -= 1
+        y0 += 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x -= 1
+            y -= 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 -= 1
+        y0 += 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x += 1
+            y += 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 -= 1
+        y0 -= 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x -= 1
+            y += 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 -= 1
+        y0 -= 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x += 1
+            y -= 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 += 1
+        y0 -= 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x -= 1
+            y -= 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 += 1
+        y0 -= 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x += 1
+            y += 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 += 1
+        y0 += 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x -= 1
+            y += 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+    for i in range(8):
+        x0 += 1
+        y0 += 1
+        x = x0
+        y = y0
+        for n in range(8):
+            x += 1
+            y -= 1
+            if x == x1 and y == y1:
+                print('Первый ход: фигура перемещена на поле ({}, {})'.format(x0 + 1, y0 + 1))
+                print('Фигура достигла поля (m, n)')
+                return 0
+    x0 = k
+    y0 = l
+        
+# Проверка на перемещение на поле (m, n) за один ход или же за два
+if name_area == 'ферзь':
+    if queen(k, l, m, n):
+        print('Можно попасть одним ходом на поле (m, n)')
+    else:
+        queen_move(k, l, m, n)
+elif name_area == 'ладья':
+    if rook(k, l, m, n):
+        print('Можно попасть одним ходом на поле (m, n)')
+    else:
+        rook_move(k, l, m, n)
+elif name_area == 'слон':
+    if eleph(k, l, m, n):
+        print('Можно попасть одним ходом на поле (m, n)')
+    else:
+        eleph_move(k, l, m, n)
     
